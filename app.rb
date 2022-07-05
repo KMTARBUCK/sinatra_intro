a2cc845db18e677078adeae74f17eb62f7258cd5
KMTARBUCK <kmtarbuck@gmail.com>
2022-07-05 20:16:35 +0100

    Sinatra

---
+++ app.rb
@@ -0,0 +1,19 @@
+# myapp.rb
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  'Hello!'
+end
+
+get '/secret' do
+  'Secret message'
+end
+
+get '/otherpage' do
+  'this is another page'
+end
+
+get '/cat' do
+"<div style='border: 10px dashed red'><img src='http://placekitten.com/500/500'></div>"
+end
